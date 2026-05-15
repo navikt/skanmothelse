61b50c842d25da6c0f440f095e88db304b699a9a
Christian Tumyr <ctumyr@gmail.com>
2026-05-15 11:48:49 +0200

    MMA-8653 Fjern ubrukt DokTimedAspect, Metrics og MetricLabels (#157)

--- app/src/main/java/no/nav/skanmothelse/ApplicationConfig.java
+++ app/src/main/java/no/nav/skanmothelse/ApplicationConfig.java
@@ -1,22 +1,17 @@
 package no.nav.skanmothelse;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import no.nav.skanmothelse.azure.AzureProperties;
 import no.nav.skanmothelse.config.properties.JiraAuthProperties;
 import no.nav.skanmothelse.config.properties.SkanmothelseProperties;
 import no.nav.skanmothelse.config.properties.SlackProperties;
-import no.nav.skanmothelse.metrics.DokTimedAspect;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @ComponentScan
 @EnableAutoConfiguration
-@EnableAspectJAutoProxy
 @EnableConfigurationProperties({
 		SkanmothelseProperties.class,
 		SlackProperties.class,
@@ -26,9 +21,4 @@ import org.springframework.resilience.annotation.EnableResilientMethods;
 @EnableResilientMethods
 @Configuration
 public class ApplicationConfig {
-
-	@Bean
-	public DokTimedAspect timedAspect(MeterRegistry registry) {
-		return new DokTimedAspect(registry);
-	}
 }
